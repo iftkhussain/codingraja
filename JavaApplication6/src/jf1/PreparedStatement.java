061ca82f625ad87e857d3031dcd6ad665a65386e
iftkhussain <120367330+iftkhussain@users.noreply.github.com>
2023-09-27 21:30:06 +0530

    Add files via upload

---
+++ JavaApplication6/src/jf1/PreparedStatement.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jf1;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author iftikhar hussain
+ */
+class PreparedStatement {
+
+    ResultSet executeQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
